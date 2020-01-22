c520f87281931699f37663fcde1a60d669ad8145
yydcnjjw <yydcnjjw@gmail.com>
2020-01-22 11:43:00 +0800

    Add initial files

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      ((cmake/build-type . "Debug")
+       (cmake/build-option . "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON")
+       (cmake/build-dir . "debug"))))
